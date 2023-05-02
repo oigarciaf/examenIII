af207df9ff9a78e16d3fac91721e93fb7e1f05d6
Frostito <120237135+Frostito@users.noreply.github.com>
2023-05-02 17:20:22 -0600

    Update Filter.tsx

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import { useState , useEffect } from 'react';
-import { Props } from '../interfaces/IFilter'
+import { IFilterProps} from '../interfaces/IFilter'
 
 
-const Filter = ({ filter, onChange }: Props) => {
+
+const Filter = ({ filter, onChange}: IFilterProps) => {
+  
   return (
     <div>
-      <label htmlFor="filter">Filtro:</label>
-      <input type="text" id="filter" value={filter} onChange={onChange} />
+      <label htmlFor="filter"> Filtro: </label>
+
+      <select id="filter" value={filter} onChange={onChange}>
+      <option value="">Seleccione un equipo</option>
+        <option value="Development">Development</option>
+        <option value="QA">QA</option>
+          <option value="PMs">PMs</option>
+          <option value="BI">BI</option>
+      </select>
+
+  
     </div>
   );
 };
 
+
+  
+
 export default Filter;
+
+
+//const Filter = ({ filter, onChange }: IFilterProps) => {
+ //   return (
+ //     <div>
+ //       <label htmlFor="filter">Filtro:</label>
+ // 
+ //       <input type="text" id="filter" value={filter} onChange={onChange} />
+ //    
+ //     </div>
+ //   );
+ // };
+
 
 
